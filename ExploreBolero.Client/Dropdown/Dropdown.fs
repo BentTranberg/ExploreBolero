3671b2f0fd4c395641cf8280cc37b7a1ae80b010
Bent Tranberg <bent.tranberg@gmail.com>
2019-10-03 10:28:32 +0200

    Halfway to creation of programmable dropdown.

--- ExploreBolero.Client/Dropdown/Dropdown.fs
+++ ExploreBolero.Client/Dropdown/Dropdown.fs
@@ -32,6 +32,29 @@ module View =
 
     type Tmpl = Template<"Dropdown/dropdown.html">
 
+    let dropdown =
+        div [ attr.``class`` "dropdown is-hoverable" ] [
+            div [ attr.``class`` "dropdown-trigger" ] [
+                button [ attr.``class`` "button"; "aria-haspopup" => "true"; "aria-controls" => "dropdown-menu" ] [
+                    span [] [ text "Dropdown button" ]
+                    span [ attr.``class`` "icon is-small" ] [
+                        i [ attr.``class`` "fas fa-angle-down"; "aria-hidden" => "true" ] []
+                    ]
+                ]
+            ]
+            div [ attr.``class`` "dropdown-menu"; attr.id "dropdown-menu"; "role" => "menu" ] [
+                div [ attr.``class`` "dropdown-content" ] [
+                    a [ attr.``class`` "dropdown-item" ] [ text "One" ]
+                    a [ attr.``class`` "dropdown-item" ] [ text "Two" ]
+                    a [ attr.``class`` "dropdown-item is-active" ] [ text "Three" ]
+                    a [ attr.``class`` "dropdown-item" ] [ text "Four" ]
+                    a [ attr.``class`` "dropdown-item" ] [ text "Five" ]
+                ]
+            ]
+        ]
+
     let page (model: Model) (dispatch: Dispatch<Message>) =
         Tmpl()
+            .Dropdown1(dropdown)
+            .Dropdown2(dropdown)
             .Elt()
